4f958ef33a3ca7c1fa9080c8565de3e14c8acb37
Yuriy Stetskyi <ystetskyy333@gmail.com>
2018-04-01 20:11:28 +0300

    Implement logging to local file system

--- FilmSearch/FilmSearch/Controllers/HomeController.cs
+++ FilmSearch/FilmSearch/Controllers/HomeController.cs
@@ -6,11 +6,24 @@ using System.Threading.Tasks;
 using FilmSearch.DAL;
 using Microsoft.AspNetCore.Mvc;
 using FilmSearch.Models;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Hosting;
 
 namespace FilmSearch.Controllers
 {
     public class HomeController : Controller
     {
+
+        private ILogger logger;
+        private IHostingEnvironment environment;
+
+        public HomeController(ILogger<HomeController> _logger, IHostingEnvironment _env)
+        {
+            logger = _logger;
+            environment = _env;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -18,6 +31,30 @@ namespace FilmSearch.Controllers
 
         public IActionResult Error()
         {
+
+            var exceptionFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+            if (exceptionFeature != null)
+            {
+                string exceptionRoute = exceptionFeature.Path;
+
+                Exception exception = exceptionFeature.Error;
+
+                while(exception.InnerException != null)
+                {
+                    exception = exception.InnerException;
+                }
+
+                CustomFileLogger.Instance.Log(
+                entry: new LogEntry()
+                {
+                    AddingTime = DateTime.Now,
+                    Data = exception,
+                    Status = LogEntryStatus.ERROR
+                },
+                dirPath: $"{environment.ContentRootPath}/logs");
+            }
+
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
---
+++ FilmSearch/FilmSearch/Models/ICustomLogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmSearch.Models
+{
+    public interface ICustomLogger
+    {
+        void Log(LogEntry entry, string dirPath);
+
+        IEnumerable<LogEntry> Extract(DateTime date, string dirPath);
+    }
+}
