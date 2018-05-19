1227e0e2e1f6443506849e470f1f440a27032eb0
Yuriy Stetskyi <ystetskyy333@gmail.com>
2018-05-19 13:59:06 +0300

    Added logic for managing genres

---
+++ FilmSearch/FilmSearch/Controllers/GenreController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FilmSearch.DAL;
+using FilmSearch.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmSearch.Controllers
+{
+    public class GenreController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GenreController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Genre> genres = _unitOfWork.GenreRepository.GetAll();
+            return View(genres);
+        }
+
+        public IActionResult Remove(long id)
+        {
+            try
+            {
+                _unitOfWork.GenreRepository.Delete(id);
+                _unitOfWork.Save();
+
+                return Ok();
+            }
+            catch(Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Add([FromBody] Genre genre)
+        {
+            try
+            {
+                IEnumerable<Genre> genres = _unitOfWork.GenreRepository.GetAll();
+
+                if(genres.Select(x => x.Name).Contains(genre.Name))
+                {
+                    throw new Exception("Genre already exists");
+                }
+
+                _unitOfWork.GenreRepository.Add(genre);
+                _unitOfWork.Save();
+
+                return Ok();
+            }
+            catch(Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+    }
+}
